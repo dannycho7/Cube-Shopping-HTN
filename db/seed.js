b51b027e4ad0dc2a2c46844d2f10d1f7a101de84
Oliver Fei <ofei006@uottawa.ca>
2017-09-16 14:34:44 -0500

    remove old index

--- db/seed.js
+++ db/seed.js
@@ -5,22 +5,106 @@ mongoose.connect("mongodb://localhost/htn", { useMongoClient: true }, () => cons
 
 Item.create([
 {
-	"name": "Shirt",
-	"price": 1000,
+	"name": "Cheap Shirt",
+	"price": 10,
 	"description": "This is a shirt",
 	"modelUrl": "...",
 	"itemType": "t-shirt"
 },
 {
-	"name": "Yeezys",
-	"price": 100000,
-	"description": "This is an overpriced shoe",
+	"name": "Somewhat Cheap Shirt",
+	"price": 11,
+	"description": "This is a shirt",
 	"modelUrl": "...",
 	"itemType": "t-shirt"
 },
 {
+	"name": "Shirt",
+	"price": 12,
+	"description": "This is a shirt",
+	"modelUrl": "...",
+	"itemType": "t-shirt"
+},
+{
+	"name": "Somewhat Expensive Shirt",
+	"price": 13,
+	"description": "This is a shirt",
+	"modelUrl": "...",
+	"itemType": "t-shirt"
+},
+{
+	"name": "Expensive Shirt",
+	"price": 14,
+	"description": "This is a shirt",
+	"modelUrl": "...",
+	"itemType": "t-shirt"
+},
+{
+	"name": "Cheap Yeezys",
+	"price": 100000,
+	"description": "This is an overpriced shoe",
+	"modelUrl": "...",
+	"itemType": "shoe"
+},
+{
+	"name": "Somewhat Cheap Yeezys",
+	"price": 100001,
+	"description": "This is an overpriced shoe",
+	"modelUrl": "...",
+	"itemType": "shoe"
+},
+{
+	"name": "Yeezys ",
+	"price": 100002,
+	"description": "This is an overpriced shoe",
+	"modelUrl": "...",
+	"itemType": "shoe"
+},
+{
+	"name": "Somewhat Expensive Yeezys",
+	"price": 100003,
+	"description": "This is an overpriced shoe",
+	"modelUrl": "...",
+	"itemType": "shoe"
+},
+{
+	"name": "Expensive Yeezys",
+	"price": 100004,
+	"description": "This is an overpriced shoe",
+	"modelUrl": "...",
+	"itemType": "shoe"
+},
+{
+	"name": "Cheap Pants",
+	"price": 20,
+	"description": "This is pants",
+	"modelUrl": "....",
+	"itemType": "pants"
+},
+{
+	"name": "Somewhat Cheap Pants",
+	"price": 21,
+	"description": "This is pants",
+	"modelUrl": "....",
+	"itemType": "pants"
+},
+{
 	"name": "Pants",
-	"price": 2500,
+	"price": 22,
+	"description": "This is pants",
+	"modelUrl": "....",
+	"itemType": "pants"
+},
+{
+	"name": "Somewhat Expensive Pants",
+	"price": 23,
+	"description": "This is pants",
+	"modelUrl": "....",
+	"itemType": "pants"
+},
+{
+	"name": "Expensive Pants",
+	"price": 24,
 	"description": "This is pants",
 	"modelUrl": "....",
 	"itemType": "pants"
