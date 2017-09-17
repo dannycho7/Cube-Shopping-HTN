2001d7bd5b485cb70741cd7fee84307b796b6a3a
dannycho7 <dannycho7@gmail.com>
2017-09-16 20:50:55 -0700

    Adjust Total text size

--- client/components/ShoppingCart/presenters/ShoppingCartTotal.js
+++ client/components/ShoppingCart/presenters/ShoppingCartTotal.js
@@ -7,7 +7,7 @@ const ShoppingCartTotal = ({ inventory, items_in_cart, position }) => {
 	});
 	return (
 		<a-entity
-			text={`value: Total: $${total}; color: black; anchor: left; wrapCount: 90` }
+			text={`value: Total: $${total}; color: black; anchor: left; wrapCount: 80` }
 			position={position}
 		/>
 	);
