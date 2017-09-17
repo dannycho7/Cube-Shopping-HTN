e15ee74e1218b701a7cf44106106827486569a8a
dannycho7 <dannycho7@gmail.com>
2017-09-17 04:58:14 -0700

    Change seed item descriptions

--- db/seed.js
+++ db/seed.js
@@ -13,99 +13,99 @@ Item.create([
 },
 {
 	"name": "Somewhat Cheap Shirt",
-	"price": 11,
-	"description": "This is a shirt",
+	"price": 4,
+	"description": "Shirts are widely available",
 	"modelUrl": "shirt_green",
 	"itemType": "t-shirt"
 },
 {
 	"name": "Shirt",
 	"price": 12,
-	"description": "This is a shirt",
+	"description": "This is a regular shirt",
 	"modelUrl": "shirt_grey",
 	"itemType": "t-shirt"
 },
 {
 	"name": "Somewhat Expensive Shirt",
-	"price": 13,
-	"description": "This is a shirt",
+	"price": 25,
+	"description": "Very high quality",
 	"modelUrl": "shirt_orange",
 	"itemType": "t-shirt"
 },
 {
 	"name": "Expensive Shirt",
-	"price": 14,
-	"description": "This is a shirt",
+	"price": 70,
+	"description": "You must shower in gold as well.",
 	"modelUrl": "shirt_red",
 	"itemType": "t-shirt"
 },
 {
-	"name": "Cheap Yeezys",
-	"price": 100000,
-	"description": "This is an overpriced shoe",
+	"name": "Fake Yeezys",
+	"price": 100,
+	"description": "This is definitely illegal",
 	"modelUrl": "shoes_blue",
 	"itemType": "shoe"
 },
 {
-	"name": "Somewhat Cheap Yeezys",
-	"price": 100001,
-	"description": "This is an overpriced shoe",
+	"name": "Somewhat Real Yeezys",
+	"price": 500,
+	"description": "This is a little sketchy",
 	"modelUrl": "shoes_brown",
 	"itemType": "shoe"
 },
 {
-	"name": "Yeezys ",
-	"price": 100002,
-	"description": "This is an overpriced shoe",
+	"name": "Yeezys",
+	"price": 1000,
+	"description": "This is a collector's item",
 	"modelUrl": "shoes_green",
 	"itemType": "shoe"
 },
 {
-	"name": "Somewhat Expensive Yeezys",
-	"price": 100003,
+	"name": "Kanye Signed these",
+	"price": 100000,
 	"description": "This is an overpriced shoe",
 	"modelUrl": "shoes_red",
 	"itemType": "shoe"
 },
 {
-	"name": "Expensive Yeezys",
-	"price": 100004,
-	"description": "This is an overpriced shoe",
+	"name": "Sketchers",
+	"price": 30,
+	"description": "This is a childhood treasure",
 	"modelUrl": "shoes_white",
 	"itemType": "shoe"
 },
 {
 	"name": "Cheap Pants",
 	"price": 20,
-	"description": "This is pants",
+	"description": "You pay for what you get",
 	"modelUrl": "pants_blue",
 	"itemType": "pants"
 },
 {
-	"name": "Somewhat Cheap Pants",
-	"price": 21,
-	"description": "This is pants",
+	"name": "Levi's Jeans",
+	"price": 50,
+	"description": "Very Durable Jeans",
 	"modelUrl": "pants_brown",
 	"itemType": "pants"
 },
 {
-	"name": "Pants",
-	"price": 22,
-	"description": "This is pants",
+	"name": "Disco Jeans",
+	"price": 70,
+	"description": "Are you looking to party?",
 	"modelUrl": "pants_green",
 	"itemType": "pants"
 },
 {
 	"name": "Somewhat Expensive Pants",
-	"price": 23,
-	"description": "This is pants",
+	"price": 150,
+	"description": "You must be headed for an interview",
 	"modelUrl": "pants_white",
 	"itemType": "pants"
 },
 {
 	"name": "Expensive Pants",
-	"price": 24,
-	"description": "This is pants",
+	"price": 250,
+	"description": "You're probably conducting interviews",
 	"modelUrl": "pants_pink",
 	"itemType": "pants"
 }], () => console.log("Created an item"));	
