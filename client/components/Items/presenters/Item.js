6b63a889eef7ffd410c5cf613642ce28a8f6b405
dannycho7 <dannycho7@gmail.com>
2017-09-17 00:03:33 -0700

    Animate items

--- client/components/Items/presenters/Item.js
+++ client/components/Items/presenters/Item.js
@@ -7,7 +7,18 @@ const Item = ({ price, name, modelUrl, position, onMousedown, inCart }) => {
 			obj-model={`obj: url(models/${modelUrl}.obj); mtl: url(models/${modelUrl}.mtl)`}
 			events={{ mousedown: onMousedown }}
 			position={position}
-		/>
+		>
+			<a-animation
+				attribute="rotation"
+            	dur="5000"
+            	easing="linear"
+            	direction={ Math.random() > 0.5 ? "normal" : "reverse" }
+            	fill="forwards"
+            	to="0 360 0"
+            	repeat="indefinite"
+            />
+        </Entity>
+			
 	);
 };
 
